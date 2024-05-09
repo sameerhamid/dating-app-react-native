dfc7e447d8eac22e59ea08f1b3cc7b47f5643dc7
inder <sameerhamid213@gmail.com>
2024-05-09 18:21:12 +0530

    added heading and top icons to name screen

--- src/container/Auth/NameScreen/index.tsx
+++ src/container/Auth/NameScreen/index.tsx
@@ -1,12 +1,74 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
+import CustomText from '../../../common/components/customText';
+import {scaleFontSize, scaleSize} from '../../../common/utils/scaleSheetUtils';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const NameScreen = () => {
   return (
-    <View>
-      <Text>NameScreen</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      {/* heading  */}
+      <View style={styles.headingContainer}>
+        <CustomText
+          text="NO BACKGROUND CHECKS ARE CONDUCTED"
+          txtSize={scaleFontSize(28)}
+        />
+      </View>
+
+      {/* rest of the screen  */}
+
+      <View style={styles.container}>
+        <View style={styles.iconsContainer}>
+          <View style={styles.newsLetterIcon}>
+            <MaterialCommunityIcons
+              name="newspaper-variant-outline"
+              size={scaleSize(22)}
+              color={'black'}
+            />
+          </View>
+          <View style={styles.dotsContainer}>
+            <View style={styles.dots} />
+            <View style={styles.dots} />
+            <View style={styles.dots} />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NameScreen;
+
+const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headingContainer: {
+    marginTop: scaleSize(40),
+    alignItems: 'center',
+  },
+  container: {
+    marginTop: scaleSize(30),
+    marginHorizontal: scaleSize(30),
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    columnGap: scaleSize(10),
+    alignItems: 'center',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    columnGap: scaleSize(6),
+  },
+  dots: {
+    width: scaleSize(8),
+    height: scaleSize(8),
+    borderRadius: scaleSize(4),
+    backgroundColor: 'black',
+  },
+  newsLetterIcon: {
+    padding: scaleSize(7),
+    borderWidth: 1,
+    borderRadius: scaleSize(30),
+  },
+});
